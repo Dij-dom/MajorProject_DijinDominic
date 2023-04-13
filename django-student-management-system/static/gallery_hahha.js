e4a07daaa9fa587b478bf8e2ec15cbfc609b83b3
CodingPanda <dijin1906@gmail.com>
2023-04-13 18:25:57 +0530

    Updated Gallery

--- django-student-management-system/static/gallery_hahha.js
+++ django-student-management-system/static/gallery_hahha.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
-    $("img").click(function(){
-    var t = $(this).attr("src");
-    $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
-    $("#myModal").modal();
-  });
-  
-  $("video").click(function(){
-    var v = $("video > source");
-    var t = v.attr("src");
-    $(".modal-body").html("<video class='model-vid' controls><source src='"+t+"' type='video/mp4'></source></video>");
-    $("#myModal").modal();  
-  });
-  });//EOF Document.ready
+// Load photos from a JSON file
+fetch('photos.json')
+	.then(response => response.json())
+	.then(data => {
+		const gallery = document.querySelector('.gallery_main');
+
+		data.forEach(photo => {
+			const img = document.createElement('img');
+			img.src = photo.url;
+			img.alt = photo.alt;
+			gallery.appendChild(img);
+		});
+	})
+	.catch(error => console.error(error));
